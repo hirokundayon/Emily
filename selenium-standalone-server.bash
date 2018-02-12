90c110169d798450e98ba91f0e660e9c35afe257
hirokundayon <hirokundayon@users.noreply.github.com>
2018-02-12 18:35:37 +0900

    Google Chromeのみの制御に変更したため不要に。

--- selenium-standalone-server.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-java \
- -Dwebdriver.chrome.driver=/path/to/chromedriver \
- -Dwebdriver.edge.driver=/path/to/MicrosoftWebDriver \
- -Dwebdriver.ie.driver=/path/to/IEDriverServer \
- -jar /path/to/selenium-server-standalone-3.3.1.jar
